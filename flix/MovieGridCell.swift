e26e58cdaed1c5933e9a18f7f2256e0819e568e2
Olivia Gillam <oliviagillam@Olivias-MacBook-Pro.local>
2021-04-14 17:18:08 -0500

    finished second part

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Olivia Gillam on 4/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet var posterView: UIImageView!
+}
